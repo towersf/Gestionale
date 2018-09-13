c9cd369a49194e19cd87019959e60885cc213a7f
towersf <towersf@gmail.com>
2018-09-13 21:38:54 +0200

    Lezione 13/09/18 Creato form articoli e ordini

--- src/GestionaleWinUI/Form1.cs
+++ src/GestionaleWinUI/Form1.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Windows.Forms;
+using GestionaleWinUI.Forms;
 
 namespace GestionaleWinUI
 {
     public partial class MainForm : Form
     {
+        private Form _formArticoli;
+        private Form _formOrdini;
+
         public MainForm()
         {
             InitializeComponent();
@@ -17,10 +21,28 @@ namespace GestionaleWinUI
 
         private void cmdArticoli_Click(object sender, EventArgs e)
         {
+            if(_formArticoli == null)
+            {
+                _formArticoli = new FormArticoli();
+                _formArticoli.Show();
+            }
+            else
+            {
+                _formArticoli.Activate();
+            }
         }
 
         private void cmdOrdini_Click(object sender, EventArgs e)
         {
+            if(_formOrdini == null)
+            {
+                _formOrdini = new FormOrdini();
+                _formOrdini.Show();
+            }
+            else
+            {
+                _formOrdini.Activate();
+            }
         }
 
         private void tsmUtenti_Click(object sender, EventArgs e)
